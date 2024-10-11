a69f512406b8c8cd32b0542325c77d3a97eecfff
AlexCurcic <curcic.alex@gmail.com>
2024-10-11 19:18:34 +0200

    models.py is updated

--- app/models.py
+++ app/models.py
@@ -12,11 +12,11 @@ class History(models.Model):
         ('withdraw', 'Withdraw'),
     ]
 
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, null=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
     amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    type = models.CharField(max_length=10, choices=TYPE_CHOICES, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    datetime = models.DateTimeField(auto_now_add=True, null=True)
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.user.username} - {self.type} - {self.amount} - {self.status}'
